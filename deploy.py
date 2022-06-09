565e7b47373193b59571e12f1246f779babc472a
Nic <nicisme@gmail.com>
2022-06-09 18:58:20 +0200

    Init Commit

---
+++ deploy.py
@@ -0,0 +1,27 @@
+from solcx import compile_standard, install_solc
+import json
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+    print(simple_storage_file)
+
+
+# Solidity source code
+install_solc("0.6.0")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
