fd7c9a70493c2fcb8e948f988199c354de957054
Nic <nicisme@gmail.com>
2022-06-21 17:02:28 +0200

    Build, Sign, Send Transaction with state change

--- deploy.py
+++ deploy.py
@@ -41,7 +41,7 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # for connecting to ganache
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 chain_id = 1337
-my_address = "0x176B136e023Ca1412d6afe3039e2B4907998315d"
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
 private_key = os.getenv("PRIVATE_KEY")
 
 
@@ -54,7 +54,7 @@ nonce = w3.eth.getTransactionCount(my_address)
 # 1. Build a transaction
 # 2. Sign
 # 3. Send
-
+# Test transaction
 transaction = SimpleStorage.constructor().buildTransaction(
     {
         "gasPrice": w3.eth.gas_price,
@@ -63,5 +63,37 @@ transaction = SimpleStorage.constructor().buildTransaction(
         "nonce": nonce,
     }
 )
-
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print("Deploying Contract...")
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
+
+# Working with the contract, you always need
+# contract address
+# contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+# Call -> Simulate making a call and getting a value (no state change)
+# Transact -> Actually make a state change
+
+# Initial value of favourite number
+
+
+# Official State change to blockchain
+print("Updating Contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated")
+print(simple_storage.functions.retrieve().call())
